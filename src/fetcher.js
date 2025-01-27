9023edcb8e64bf4d313bb300999b29d12232ee14
sumitkk135 <sumitkk135@gmail.com>
2025-01-27 21:28:47 +0530

    new api added

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,8 +1,8 @@
-const baseUrl =  "http://localhost:3000"
+const baseUrl =  "https://api.techmavericks.in.net/api/ps/v1"
 
 export const fetcher = async (workUrl)=>{
  try {
-        const response = await fetch( workUrl )
+        const response = await fetch( baseUrl+workUrl )
         const responseData = await response.json()
         return responseData
  } catch (error) {
